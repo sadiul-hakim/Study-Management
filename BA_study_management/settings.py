682997ffcf4ea6e3cce307f688247adaf3f251a9
Sadiul Hakim <sadiulhakim@gmail.com>
2026-08-26 13:56:47 +0600

    dependency

--- BA_study_management/settings.py
+++ BA_study_management/settings.py
@@ -262,6 +262,9 @@ LANGUAGES = [
 ]
 
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
